bf1744a261837993d3b21557908310d4c72f40f6
tsato <tsato@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-23 06:09:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/throws-spam-away/tags/1.2@414024 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,55 @@
+=== Plugin Name ===
+Contributors: tsato
+Donate link: http://iscw.jp/
+Tags: comments, spam
+Requires at least: 3.1
+Tested up to: 3.2.1
+Stable tag: 1.2
+
+コメント内に日本語文字列が一つも存在しない場合あたかも受け付けたように振る舞いながらも無視
+
+== Description ==
+コメント欄に日本語文字列が含まれていないと投稿出来ない・・・といってもエラーにするのではなく「無視」して何事もなかったようにもとの記事に戻る。
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `throws-spam-away` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+= What about foo bar? =
+
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.2 =
+設定画面を設けました。こちらで、日本語文字が何文字以上入っていないとNGかのしきい値とちらっと現れるエラー文言画面から元の記事に戻ってくる時間（ミリ秒）を設定することが出来ます。
+
+= 1.1 =
+マルチバイト文字が存在していても日本語文字列を含まないとNGとするよう
+正規表現を入れて精査するように修正
+
+= 1.0 =
+新規作成
+
+== Upgrade Notice ==
+= 1.2 =
+日本語が1文字でも含まれると通ってしまうのを何文字以上含まれていないといけないのかを設定出来るようにしました。
+
+= 1.1 =
+正規表現を使いロシア語や他のマルチバイト文字列を使用する言語からの攻撃に対応
+
+= 1.0 =
+とりあえず作りました！
+== Arbitrary section ==
+
+== A brief Markdown Example ==
---
+++ trunk/throws_spam_away.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ Plugin Name: Throws SPAM Away
+ Plugin URI: http://iscw.jp/wp/
+ Description: コメント内に日本語の記述が一つも存在しない場合はあたかも受け付けたように振る舞いながらも捨ててしまうプラグイン
+ Author: 株式会社アイ・エス・シー　さとう　たけし
+ Version: 1.2
+ Author URI: http://iscw.jp/
+ */
+
+class ThrowsSpamAway {
+	var $version = '1.0';
+	function ThrowsSpamAway() {
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+	}
+
+	function comment_form() {
+		// 注意文言表示
+		echo '<div id="throwsSpamAway">';
+		echo '日本語が含まれない投稿は無視されますのでご注意ください。（スパム対策）';
+		echo '</div>';
+		return true;
+	}
+
+	function comment_post($id) {
+		global $newThrowsSpamAway;
+		global $user_ID;
+
+		if( $user_ID ) {
+			return $id;
+		}
+
+		$comment = $_POST["comment"];
+		if ($newThrowsSpamAway->validation($comment)) {
+			return $id;
+		}
+		wp_die( __('日本語を規定文字数以上含まない記事は投稿できませんよ。<script type="text/javascript">window.setTimeout(location.href = "'.$_SERVER['HTTP_REFERER'].'", '.(get_option('back_content_second')!=null?get_option('back_content_second'):10).');</script>', 'throws-spam-away'));
+	}
+
+	/**
+	 * 日本語が含まれているかチェックメソッド
+	 * @param string $comment
+	 */
+	function validation($comment) {
+		global $newThrowsSpamAway;
+		// まずはシングルバイトだけならエラー
+		if (strlen(bin2hex($comment)) / 2 == mb_strlen($comment)) {
+			return false;
+		} else {
+			// マルチバイト文字が含まれている場合は日本語が含まれていればOK
+			$count_flg = 0;
+			mb_regex_encoding('UTF-8');
+			$com_split = $newThrowsSpamAway->mb_str_split($comment);
+			foreach ($com_split as $it) {
+				if (preg_match('/[一-龠]+/u', $it)){ $count_flg += 1; }
+				if (preg_match('/[ァ-ヶー]+/u', $it)){ $count_flg += 1; }
+				if (preg_match('/[ぁ-ん]+/u', $it)){ $count_flg += 1; }
+			}
+			return ((get_option('tsa_japanese_string_min_count')!= null?
+					intval(get_option('tsa_japanese_string_min_count')):0) < $count_flg);
+		}
+	}
+	
+	function mb_str_split( $string ) { 
+		return preg_split('/(?<!^)(?!$)/u', $string ); 
+	} 
+	
+	/**
+	 * Callback admin_menu
+	 */
+	function admin_menu() {
+		if ( function_exists( 'add_options_page' ) AND current_user_can( 'manage_options' ) ) {
+			// add options page
+			$page = add_options_page( __( 'Throws SPAM Away', $this->domain ), __( 'Throws SPAM Away', $this->domain ),
+								'manage_options', __FILE__, array( $this, 'options_page' ) );
+		}
+	}
+
+	/**
+	 * Admin options page
+	 */
+	function options_page() {
+?>
+<div class="wrap">
+	<h2>Throws SPAM Away. Setting</h2>
+<form method="post" action="options.php">
+<?php wp_nonce_field('update-options'); ?>
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row">日本語文字列含有数<br />（この文字列に達していない場合無視対象となります。）</th>
+		<td><input type="text" name="tsa_japanese_string_min_count" value="<?php echo get_option('tsa_japanese_string_min_count'); ?>" /></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">元の記事に戻ってくる時間<br />（ミリ秒）</th>
+		<td><input type="text" name="tsa_back_content_second" value="<?php echo get_option('tsa_back_content_second');?>" /></td>
+	</tr>	
+</table>
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="tsa_japanese_string_min_count,tsa_back_content_second" />
+<p class="submit">
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+</form>
+		<div class="clear"></div>
+	</div>
+<?php
+	}
+}
+
+$newThrowsSpamAway = new ThrowsSpamAway;
+add_action('comment_form', array(&$newThrowsSpamAway, "comment_form"), 9999);
+add_action('pre_comment_on_post', array(&$newThrowsSpamAway, "comment_post"), 9999);
+?>
