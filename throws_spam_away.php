1eef7503039fb7ebf8f055476c72cdb8df877668
tsato <tsato@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-23 06:05:50 +0000

    日本語文字列含有最低文字数とエラー文言から戻ってくる時間（ミリ秒）設定可能にしました。
    git-svn-id: https://plugins.svn.wordpress.org/throws-spam-away/trunk@414021 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- throws_spam_away.php
+++ throws_spam_away.php
@@ -11,6 +11,7 @@
 class ThrowsSpamAway {
 	var $version = '1.0';
 	function ThrowsSpamAway() {
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 	}
 
 	function comment_form() {
@@ -33,7 +34,7 @@ class ThrowsSpamAway {
 		if ($newThrowsSpamAway->validation($comment)) {
 			return $id;
 		}
-		wp_die( __('日本語を含まない記事は投稿できませんよ。<script type="text/javascript">window.setTimeout(location.href = "'.$_SERVER['HTTP_REFERER'].'", 100);</script>', 'throws-spam-away'));
+		wp_die( __('日本語を規定文字数以上含まない記事は投稿できませんよ。<script type="text/javascript">window.setTimeout(location.href = "'.$_SERVER['HTTP_REFERER'].'", '.(get_option('back_content_second')!=null?get_option('back_content_second'):10).');</script>', 'throws-spam-away'));
 	}
 
 	/**
@@ -41,18 +42,68 @@ class ThrowsSpamAway {
 	 * @param string $comment
 	 */
 	function validation($comment) {
+		global $newThrowsSpamAway;
 		// まずはシングルバイトだけならエラー
 		if (strlen(bin2hex($comment)) / 2 == mb_strlen($comment)) {
 			return false;
 		} else {
 			// マルチバイト文字が含まれている場合は日本語が含まれていればOK
-			$flg = false;
+			$count_flg = 0;
 			mb_regex_encoding('UTF-8');
-			if (preg_match('/[一-龠]+/u', $comment)){ $flg = true; }
-			if (preg_match('/[ァ-ヶー]+/u', $comment)){ $flg = true; }
-			if (preg_match('/[ぁ-ん]+/u', $comment)){ $flg = true; }
-			return $flg;
+			$com_split = $newThrowsSpamAway->mb_str_split($comment);
+			foreach ($com_split as $it) {
+				if (preg_match('/[一-龠]+/u', $it)){ $count_flg += 1; }
+				if (preg_match('/[ァ-ヶー]+/u', $it)){ $count_flg += 1; }
+				if (preg_match('/[ぁ-ん]+/u', $it)){ $count_flg += 1; }
+			}
+			return ((get_option('tsa_japanese_string_min_count')!= null?
+					intval(get_option('tsa_japanese_string_min_count')):0) < $count_flg);
 		}
+	}
+	
+	function mb_str_split( $string ) { 
+		return preg_split('/(?<!^)(?!$)/u', $string ); 
+	} 
+	
+	/**
+	 * Callback admin_menu
+	 */
+	function admin_menu() {
+		if ( function_exists( 'add_options_page' ) AND current_user_can( 'manage_options' ) ) {
+			// add options page
+			$page = add_options_page( __( 'Throws SPAM Away', $this->domain ), __( 'Throws SPAM Away', $this->domain ),
+								'manage_options', __FILE__, array( $this, 'options_page' ) );
+		}
+	}
+
+	/**
+	 * Admin options page
+	 */
+	function options_page() {
+?>
+<div class="wrap">
+	<h2>Throws SPAM Away. Setting</h2>
+<form method="post" action="options.php">
+<?php wp_nonce_field('update-options'); ?>
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row">日本語文字列含有数<br />（この文字列に達していない場合無視対象となります。）</th>
+		<td><input type="text" name="tsa_japanese_string_min_count" value="<?php echo get_option('tsa_japanese_string_min_count'); ?>" /></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row">元の記事に戻ってくる時間<br />（ミリ秒）</th>
+		<td><input type="text" name="tsa_back_content_second" value="<?php echo get_option('tsa_back_content_second');?>" /></td>
+	</tr>	
+</table>
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="tsa_japanese_string_min_count,tsa_back_content_second" />
+<p class="submit">
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+</form>
+		<div class="clear"></div>
+	</div>
+<?php
 	}
 }
 
