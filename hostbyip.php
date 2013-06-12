ffacba78805ffa7f24728002d6d872a7ac204e3b
tsato <tsato@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-12 07:51:54 +0000

    制御IPアドレスの入力方式を「カンマ区切り」から「改行」へ変更しました。 従来のデータ（カンマ区切り）は自動的に改行区切りに変更されます。
    スパム投稿の一覧を見やすくしました。
    　　これまでスパム投稿情報の表示が日付が一覧で縦表示していたため見づらいものとなっておりました。
    　　今回、横軸にその情報を追加しました。
    　　また、スパム投稿リストの表示も改良し、IPアドレス・スパム投稿数・最終投稿日でソートを掛けられるようにしました。
    
    スパムコメント情報のIP一覧からIPアドレスをクリックした際に表示していた情報を下記のようにしました。
    ・IPアドレス
    ・ホスト名が特定出来た場合はホスト名
    ・このIPアドレスからの最終投稿日時
    ・このIPアドレスからスパム投稿を受けた記事（リンク）
    
    スパム情報収集機能を「スパムコメント情報を保存する」にした場合に限り、「一定時間内スパム認定機能」が作動するように出来ます。
    	この「○」を設定します→《○分以内に○回スパムとなったら○分間、当該IPからのコメントはスパム扱いする設定》
    	例えば、「１０分以内に２回スパムとなったら１０分間、当該IPからのコメントはスパム扱いする」とした場合は
    	過去（今から）１０分前から今まで２回スパム扱いとなったIPからの投稿は３回目以降スパム扱いとなります。
    	そのため、１０分以内に３回目のスパムを作ってしまったためそこから１０分以上経過しないとずっとスパム扱いになってしまいます。
    	今までのように「これは・・・・ダメ」「これは・・・・ダメ」「これは・・・・行けた！」「よっしゃーー！突っ込めー！」という風にはできなくなるということです。
    	上記の例はデフォルト値ですが、「１時間（60分）以内に３回スパムになったら２４時間（1440分）スパム扱い」という設定の場合は
    	24時間以内に3回スパムがあったら・・・という設定とほぼ同等（完全に同等ではありませんが・・・）ですので
    	24時間の方に合わせる形で24時間以内のスパム回数を算出するようにしています。（今後見直すかもしれません）
    	あまり長い時間を指定しますとコメント欄への反映が遅くなりますので気をつけてください。
    	５～１０分程度を推奨します。
    
    IPアドレス指定を「カンマ区切り」から「改行区切り」へ変更。スパム投稿IPアドレス情報に下記データ追加
        ○IPアドレス
        ○ホスト名が特定出来た場合はホスト名
        ○このIPアドレスからの最終投稿日時
        ○このIPアドレスからスパム投稿を受けた記事（リンク）
    
    
    git-svn-id: https://plugins.svn.wordpress.org/throws-spam-away/trunk@725580 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hostbyip.php
+++ hostbyip.php
@@ -1,8 +1,21 @@
 <?php
 /**
+ * <p>ThrowsSpamAway</p> hostbyipページ
+ * WordPress's Plugin
+ * @author Takeshi Satoh@GTI Inc. 2013
+ */
+require_once 'throws_spam_away.class.php';
+require_once dirname( __FILE__ ).'/../../../wp-load.php';
+/**
  * ホスト検索
  */
 $spam_ip = htmlspecialchars($_GET['ip']);
+$newThrowsSpamAway = new ThrowsSpamAway(TRUE);
+$last_spam_comment_result = $newThrowsSpamAway->get_last_spam_comment($spam_ip);
+// 最終投稿日
+$last_comment_date = $last_spam_comment_result->post_date;
+$last_comment_post = get_permalink($last_spam_comment_result->post_id);
+$last_comment_post_title = get_the_title(get_post($last_spam_comment_result->post_id));
 ?>
 <!DOCTYPE html>
 <!--[if IE 8]>
@@ -22,11 +35,12 @@ window.onblur=function(){
 </head>
 <body>
 <div style="textalign: center;">
+<h2 style="background:#333; color:#fff;"><?php echo $spam_ip; ?></h2>
 <?php
 $spam_host = gethostbyaddr(htmlspecialchars($spam_ip));
 if ($spam_host != $spam_ip) {
 ?>
-特定のホスト情報が見つかりました。<br />
+<h3 style="background: #666;color: #fff;">特定のホスト情報が見つかりました。</h3>
 ↓↓↓<br />
 
 <h4><?php echo $spam_host; ?></h4>
@@ -34,12 +48,15 @@ Whois: <a href="http://whois.arin.net/rest/ip/<?php echo $spam_ip; ?>" target="_
 <?php
 } else {
 ?>
-特定のホスト情報は見つかりませんでした。
+<h3 style="background: #666;color: #fff;">このIPアドレスから特定のホスト情報は見つかりませんでした。</h3>
 <?php
 }
 ?>
-
-<a href="javascript:void(0);" onclick="window.close();">閉じる</a>
+<?php if ( $last_spam_comment_result != NULL ) { ?>
+<div style="background: #999;color: #fff;margin:3px 0 0 0;">このIPからの最終投稿日時</div><?php echo $last_comment_date; ?><br />
+<div style="background: #999;color: #fff;margin:3px 0 0 0;">このIPからスパム投稿対象となったページ</div><a href="<?php echo $last_comment_post; ?>" target="_blank"><?php echo $last_comment_post_title; ?></a><br />
+<?php } ?>
+<div style="text-align:right;"><a href="javascript:void(0);" onclick="window.close();">閉じる</a></div>
 </div>
 </body>
 </html>
--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,8 @@
 <?php
 /*
 Throws SPAM Awayプラグインアンインストール
-2.3追加
+2.3 追加
+2.4 追記
 */
 
 if(!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN')) { exit(); }
@@ -29,4 +30,9 @@ delete_option('tsa_url_count_over_error_message');
 // ver.2.3
 delete_option('tsa_meta_version');
 delete_option('tsa_spam_data_save');
-?>
+// ver.2.4
+delete_option('tsa_spam_limit_flg');
+delete_option('tsa_spam_limit_minutes');
+delete_option('tsa_spam_limit_count');
+delete_option('tsa_spam_limit_over_interval');
+delete_option('tsa_spam_limit_over_interval_error_message');
