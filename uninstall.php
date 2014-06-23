aaa9c7b0cf4abd404f5b926ac2f7e098ff26c8bc
tsato <tsato@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-23 07:33:42 +0000

    ・バージョン2.6.4 　2014/06/23 　タイトルの文字列をそのまま流用し「日本語」を使うことでフィルタを回避しようとする方法を排除するため 　タイトル文字列を排除してカウントする機能を追加。（デフォルトで効くようになっています。） 　「日本語文字列含有数」については、この「タイトル」に使用されている文字列を除いた数をカウントするようになります。
    　例）タイトルに使用された文字は「えーびーしー」という文字列の場合、「え」「び」「し」「ー」が何度出現してもカウントされません。
    　　　「びしえー」というように文字列を入れ替えても「えー　びー　しー」とスペースを入れても関係なくカウントされなくなります。
    
    　JSファイルの $ を jQuery に変更。
    
    git-svn-id: https://plugins.svn.wordpress.org/throws-spam-away/branches/2.6.4@936955 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Throws SPAM Awayプラグインアンインストール
+2.3 追加
+2.4 追記
+2.6 追記
+*/
+
+if(!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN')) { exit(); }
+
+delete_option('tsa_back_content_second');	// ver.1.6以下の方用
+delete_option('tsa_on_flg');
+delete_option('tsa_japanese_string_min_count');
+delete_option('tsa_back_second');
+delete_option('tsa_caution_message');
+delete_option('tsa_error_message');
+delete_option('tsa_ng_keywords');
+delete_option('tsa_ng_key_error_message');
+delete_option('tsa_must_keywords');
+delete_option('tsa_must_key_error_message');
+delete_option('tsa_tb_on_flg');
+delete_option('tsa_tb_url_flg');
+// ver.2.0
+delete_option('tsa_ip_block_from_spam_chk_flg');
+delete_option('tsa_block_ip_addresses');
+delete_option('tsa_block_ip_address_error_message');
+// ver.2.2
+delete_option('tsa_url_count_on_flg');
+delete_option('tsa_ok_url_count');
+delete_option('tsa_url_count_over_error_message');
+// ver.2.3
+delete_option('tsa_meta_version');
+delete_option('tsa_spam_data_save');
+// ver.2.4
+delete_option('tsa_spam_limit_flg');
+delete_option('tsa_spam_limit_minutes');
+delete_option('tsa_spam_limit_count');
+delete_option('tsa_spam_limit_over_interval');
+delete_option('tsa_spam_limit_over_interval_error_message');
+// ver.2.5
+delete_option('tsa_spam_champuru_flg');
+delete_option('tsa_spam_keep_day_count');
+delete_option('tsa_spam_data_delete_flg');
+delete_option('tsa_white_ip_addresses');
+// ver.2.5.1
+delete_option('tsa_caution_msg_point');
+// ver.2.6
+delete_option('tsa_empty_param_field_flg');
+delete_option('tsa_memo');
